13db590199af75892088dca233b4c0955bb14eab
JohnPolvorosa <johnpolvorosa@ontariotechu.net>
2020-04-22 19:49:58 -0400

    add href links to read more with truncate text on home page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -27,25 +28,30 @@ app.get("/", function(req,res) {
   res.render("home", { homePageText : homeStartingContent, homePosts : posts });
 });
 
+// Check for posts when entered url
 app.get("/posts/:postName", function(req,res) {
-  let requestTitle = req.params.postName;
-
-  console.log("Title requested: " + requestTitle);
+  let requestTitle = _.lowerCase(req.params.postName);
+  // Iterate through array to check
   posts.forEach(function(p) {
-    // console.log("Title checking: " + p.title);
-    if (requestTitle === p.title) {
-      console.log("Match found!");
+    // Lower case both
+    let storedTitles = _.lowerCase(p.title);
+    if (requestTitle === storedTitles) {
+      res.render("post", { postTitle: p.title, postContent: p.text });
     } else {
       console.log("No match found");
     }
   });
-
 });
 //Playing with express routing DOES WORK if you specify number of parameters@#@#@#!#
 app.get("/posts/:postName/user/:userName", function(req,res) {
   console.log(req.params.postName);  
   console.log(req.params.userName);
 });
+
+// app.post("/posts/:postName", function(req,res) {
+//   res.render("post", { postTitle: p.title, postContent: p.text })
+// })
+
 
 app.get("/about", function(req,res) {
   res.render("about", { aboutPageText : aboutContent });
